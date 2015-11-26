d4c631dd07a649958d78ee7321a9757872e39d19
rossanthony <ross@velo33.com>
2015-11-26 01:17:17 +0000

    PR algorithm working... now handling dangling pages

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sewn-pagerank
+
+
+Dangling links are simply links that point to any page with no outgoing links. They affect the model because it is not clear where their weight should be distributed, and there are a large number of them. Often these dangling links are simply pages that we have not downloaded yet.
+
+Because dangling links do not affect the ranking of any other page directly, we simply remove them from the system until all the PageRanks are calculated. After all the PageRanks are calculated they can be added back in without affecting things significantly.
--- pagerank.py
+++ pagerank.py
@@ -1,5 +1,7 @@
 
-from collections import defaultdict # dictionary data structures
+from collections import OrderedDict
+import math
+import urllib
 import numpy as np
 import urlparse              # lib for parsing urls
 from urlparse import urljoin # allows conversion of relative urls into absolute ones 
@@ -10,20 +12,64 @@ class PageRank(object):
 
     def __init__(self):
         # Data structures
-        self.crawledPages  = defaultdict(list)  # holds Crawled Pages (read in from file)
+        self.visitedPages = []
+        self.linksOnPages = {}
+        self.crawledPages  = {} # holds Crawled Pages (read in from file)
         self.totalOutlinks = 0
+        
+        self.teleportationFactor = 1.0
+        self.convergence = 0.0001 
+        self.dp = 4 # decimal places to round PR's to
 
+        self.stats  = "Teleportation factor: %d\n" % self.teleportationFactor
+        self.stats += "Convergence threashold: +/-%.4f\n" % self.convergence
+        
         self.readInCrawlTxt()
-        #print self.matrix
-        print json.dumps(self.crawledPages, sort_keys=True, indent=4)
         
-        print "Total Visited Pages: %d" % len(self.crawledPages)
-        print "Total Outlinks: %s" % self.totalOutlinks
-        
-        self.saveOutput()
-        self.saveLinks()
+        # self.saveOutput()
+        # self.saveLinks()
         self.buildMatrix()
-        print self.matrix
+        self.saveMatrixAsCsv()
+        self.calcTotals()
+        self.initPageRanks()
+        self.calcPageRanks()
+        self.outputStats()
+
+
+    #
+    # Returns: Void
+    #
+    def calcTotals(self):
+        rowTotals = self.matrix.sum(axis=1) # rows
+        columnTotals = self.matrix.sum(axis=0) # cols
+        self.inlinks = {}
+        self.inlinkCounts = {}
+        self.outlinkCounts = {}
+        self.totalInlinks = 0
+        self.totalOutlinks = 0
+        index = 0
+        for page in self.visitedPages:
+            self.inlinks[page] = self.findAllPagesWithInlinksToUrl(page)
+            self.inlinkCounts[page]  = columnTotals.item(index)
+            self.outlinkCounts[page] = rowTotals[index].item(0)
+            self.totalInlinks  += int(columnTotals.item(index))
+            self.totalOutlinks += int(rowTotals[index].item(0))
+            index+=1
+        # Get the total num of non-dangling pages    
+        self.nonDanglingPages = 0;
+        for page, outlinks in self.linksOnPages.items():
+            if len(outlinks) > 0:
+                self.nonDanglingPages += 1
+
+
+    def findAllPagesWithInlinksToUrl(self, searchFor):
+        inlinksFound = []
+        for page, linksOnPage in self.linksOnPages.items():
+            if searchFor in linksOnPage:
+                inlinksFound.append(page)
+                # print page
+                # print linksOnPage
+        return inlinksFound
 
 
     #
@@ -37,14 +83,16 @@ class PageRank(object):
                 if not line:
                     break
                 if line[:8] == 'Visited:':
-                    visited = line[9:].strip().rstrip('/')
-                    if self.isStoredUrl(visited):
-                        self.crawledPages[visited] = []
+                    visited = urllib.unquote(line[9:].strip().rstrip('/'))
+                    if self.isUniqueUrl(visited):
+                        self.visitedPages.append(visited)
+                        self.linksOnPages[visited] = []
                         addLinks = True
                     else:
                         addLinks = False
                 if addLinks and line[:9] == '    Link:':
-                    self.crawledPages[visited].append(self.getAbsoluteUrl(visited, line[10:].strip()))
+                    # print self.getAbsoluteUrl(visited, line[10:].strip())
+                    self.linksOnPages[visited].append(self.getAbsoluteUrl(visited, line[10:].strip().rstrip('/')))
                     self.totalOutlinks+=1
 
 
@@ -54,13 +102,194 @@ class PageRank(object):
     #
     def buildMatrix(self):
         allPages = []
-        for rowPage in self.crawledPages:
+        for rowVisitedUrl in self.visitedPages:
             row = []
-            for pageToCheck in self.crawledPages:
-                if pageToCheck in self.crawledPages[rowPage]: row.append(1)
+            for pageToCheck in self.visitedPages:
+                if pageToCheck in self.linksOnPages[rowVisitedUrl]: row.append(1)
                 else: row.append(0)
             allPages.append(row)
         self.matrix = np.matrix(allPages)
+
+
+    def initPageRanks(self):
+        # initialise
+        self.pageRanks = {}
+        for page in self.visitedPages:
+            self.pageRanks[page] = []
+            # print "dangling page rank: %f" % (1.0 / len(self.visitedPages))
+            self.pageRanks[page].append(1) 
+
+        totalPagerank = 0.0
+        for page in self.visitedPages:
+            totalPagerank = totalPagerank + self.pageRanks[page][0]
+        print "Iteration: 0, sum of all pageranks: %f" % totalPagerank
+        # print json.dumps(self.pageRanks, sort_keys=True, indent=4)
+
+
+
+    def calcPageRanks(self):
+        iteration = 0
+        convergedCount = 0
+
+        while convergedCount < len(self.visitedPages):
+            iteration+=1
+            totalPagerank = 0.0
+
+            for page in self.visitedPages:
+                inlinks = self.inlinks[page]
+                outlinks = self.linksOnPages[page]
+
+                if len(outlinks) > 0:
+                    pageRanksOfInlinks = []
+                    for link in inlinks:
+                        pageRanksOfInlinks.append( self.pageRanks[link][iteration-1] / self.outlinkCounts[link] )
+
+                    # random surfer version of PR algorithm
+                    rank = self.teleportationFactor / self.nonDanglingPages + (1 - self.teleportationFactor) * sum(pageRanksOfInlinks)
+                else:
+                    # print "page %s has no outlinks" % page
+                    # print "using previous rank: %f" % self.pageRanks[page][iteration-1]
+                    rank = self.pageRanks[page][iteration-1]
+
+                self.pageRanks[page].append(round(rank, self.dp))    
+                totalPagerank = totalPagerank + rank
+            
+            # check for convergence
+            convergedCount = 0
+            for page in self.visitedPages:
+                if self.pageRanks[page][iteration] >= self.pageRanks[page][iteration-1] - self.convergence:
+                    convergedCount+=1
+                # else:
+                #     print "Iteration = %d, page %s not converged" % (iteration, page)
+
+        print "Reached convergence of non-dangling pages in %d iterations" % iteration
+
+        print "Now dealing with dangling links..."
+        convergedCount = 0
+
+        while convergedCount < len(self.visitedPages):
+            iteration+=1
+            totalPagerank = 0.0
+
+            for page in self.visitedPages:
+                inlinks = self.inlinks[page]
+                outlinks = self.linksOnPages[page]
+
+                if len(outlinks) == 0:
+                    pageRanksOfInlinks = []
+                    for link in inlinks:
+                        pageRanksOfInlinks.append( self.pageRanks[link][iteration-1] / self.outlinkCounts[link] )
+
+                    # random surfer version of PR algorithm
+                    rank = self.teleportationFactor / len(self.visitedPages) + (1 - self.teleportationFactor) * sum(pageRanksOfInlinks)
+                else:
+                    rank = self.pageRanks[page][iteration-1]
+
+                self.pageRanks[page].append(round(rank, self.dp))    
+                totalPagerank = totalPagerank + rank
+            
+            # check for convergence
+            convergedCount = 0
+            for page in self.visitedPages:
+                if self.pageRanks[page][iteration] >= self.pageRanks[page][iteration-1] - self.convergence:
+                    convergedCount+=1
+
+        print "Reached convergence in %d iterations" % iteration
+        print "DONE!\n..."
+
+        filename = "pagerank%.2f_workings" % self.teleportationFactor
+        filename = filename.replace('.','') + ".txt"
+        self.stats += "PR iteration trace saved to: `%s`\n" % filename
+        crawlerFile = open(filename, "w")
+        crawlerFile.write(str(json.dumps(self.pageRanks, sort_keys=True, indent=4)))
+        crawlerFile.close()
+
+        finalPageRanks = {}
+        for page in self.pageRanks:
+            finalPageRanks[page] = self.pageRanks[page][iteration]
+
+        filename = "pagerank%.2f" % self.teleportationFactor
+        filename = filename.replace('.','') + ".txt"
+        self.stats += "Final PR values saved to: `%s`\n" % filename
+        crawlerFile = open(filename, "w")
+        orderedPageRanks = "# iterations: %d\n" % iteration
+
+        for key, value in sorted(finalPageRanks.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+            orderedPageRanks += "\t%s\t%s\n" % (key, value)
+
+        crawlerFile.write(orderedPageRanks)
+        crawlerFile.close()
+        # print json.dumps(finalPageRanks, sort_keys=True, indent=4)
+
+
+    def outputStats(self):
+        # a) The number of inlinks to each page.
+        crawlerFile = open('inlinksPerPage.txt', "w")
+        inlinksPerPage = "# inlinks per page\n"
+        for key, value in sorted(self.inlinkCounts.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+            inlinksPerPage += "%d\t%s\n" % (value, key)
+        crawlerFile.write(inlinksPerPage)
+        crawlerFile.close()
+    
+        # b) The number of outlinks from each page.
+        crawlerFile = open('outlinksPerPage.txt', "w")
+        outlinksPerPage = "# outlinks per page\n"
+        for key, value in sorted(self.outlinkCounts.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+            outlinksPerPage += "%d\t%s\n" % (value, key)
+        crawlerFile.write(outlinksPerPage)
+        crawlerFile.close()
+
+        # c) The average (mean) number of inlinks to a page, variance and the standard deviation.
+        # d) The average (mean) number of outlinks from a page, variance and the standard deviation.
+        self.stats += "Total (unique) visited pages: %d (of which %d are dangling)\n" % (len(self.visitedPages), len(self.visitedPages) - self.nonDanglingPages)
+        self.stats += "Total number of inlinks: %d | outlinks: %d\n" % (self.totalInlinks, self.totalOutlinks)
+        self.stats += "Mean number of inlinks: %d | outlinks: %d\n" % (int(self.totalInlinks) / int(len(self.visitedPages)), int(self.totalOutlinks) / int(len(self.visitedPages)))
+        self.stats += "Mean number of inlinks: %d | outlinks: %d\n" % (int(self.totalInlinks) / int(len(self.visitedPages)), int(self.totalOutlinks) / int(len(self.visitedPages)))
+        
+        # To calculate the variance follow these steps: 
+        # Work out the Mean (the simple average of the numbers) 
+        # Then for each number: subtract the Mean and square the result (the squared difference). 
+        # Then work out the average of those squared differences.
+        inlinkVariance = []
+        for inlink, count in self.inlinkCounts.items():
+            inlinkVariance.append((count - (int(self.totalInlinks) / int(len(self.visitedPages)))) * (count - (int(self.totalInlinks) / int(len(self.visitedPages)))))
+        inlinkVariance = sum(inlinkVariance) / float(len(inlinkVariance))
+
+        outlinkVariance = []
+        for inlink, count in self.outlinkCounts.items():
+            outlinkVariance.append((count - (int(self.totalOutlinks) / int(len(self.visitedPages)))) * (count - (int(self.totalOutlinks) / int(len(self.visitedPages)))))
+        outlinkVariance = sum(outlinkVariance) / float(len(outlinkVariance))
+
+        self.stats += "Variance of inlinks: %d | outlinks: %d\n" % (inlinkVariance, outlinkVariance)
+        self.stats += "Standard deviation of inlinks: %d | outlinks: %d\n" % (math.sqrt(inlinkVariance), math.sqrt(outlinkVariance))
+
+        # e) The degree distribution of inlinks and outlinks, i.e. tables such as the following:
+        self.stats += "Degree of distribution saved to: inlinksPerPage.txt, outlinksPerPage.txt\n"
+        
+        print self.stats
+        
+
+    # Save the link matrix as a csv
+    # 
+    # Returns: String (url)
+    #
+    def saveMatrixAsCsv(self):
+        index = 0
+        csv = ' ,'
+        for visitedUrl in self.visitedPages:
+            csv += visitedUrl + ','
+        csv += "\n"
+        for visitedUrl in self.visitedPages:
+            csv += visitedUrl + ','
+            # print self.matrix[index].tolist().pop()
+            for val in self.matrix[index].tolist().pop():
+                csv +=  "%d," % val
+            csv += "\n"
+            index+=1
+            
+        csvMatrixFile = open("matrix.csv", "w")
+        csvMatrixFile.write(str(csv))
+        csvMatrixFile.close()
 
 
     # Convert a URL into absolute
@@ -68,36 +297,40 @@ class PageRank(object):
     # Returns: String (url)
     #
     def getAbsoluteUrl(self, currentPageUrl, outlink):
-        return urljoin(currentPageUrl, outlink).replace('/../', '/')
+        return urllib.unquote(urljoin(currentPageUrl, outlink).replace('/../', '/')).lower()
 
 
     # Check if a given url has already been stored
     #
     # Returns: Bool
     #
-    def isStoredUrl(self, url):
-        for pageUrl in self.crawledPages:
+    def isUniqueUrl(self, url):
+        for pageUrl in self.visitedPages:
             if pageUrl == url:
                 return False
         return True
 
 
     def saveOutput(self):
-        output = json.dumps(self.crawledPages, sort_keys=True, indent=4)
+        output = ''
+        for visitedUrl in self.visitedPages:
+            output += "\"%s\": " % visitedUrl
+            output += json.dumps(self.linksOnPages[visitedUrl], sort_keys=True, indent=4)
+            output += ",\n"
+        output += "\n"
         crawlerFile = open("output.txt", "w")
         crawlerFile.write(str(output))
         crawlerFile.close()
         return True
 
+
     def saveLinks(self):
         output = ''
-        for pageUrl in self.crawledPages:
+        for pageUrl in self.visitedPages:
             output += "%s\n" % pageUrl
         crawlerFile = open("visited-pages.txt", "w")
         crawlerFile.write(str(output))
         crawlerFile.close()
         return True
-
-
 
 PageRank()
